14f02cef2f2a7581a229d7e2daac04dca82a74a8
harshisawesome <hxj6205@rit.edu>
2015-09-22 04:16:42 -0400

    Fixed colon
    Colon --> Semicolon

--- admin/js/prettyHTML.js
+++ admin/js/prettyHTML.js
@@ -2,7 +2,7 @@
 function prettyHTML()
 {
     str.replace("chairman","Chairman");
-    str.replace("eboard in general", "Eboard in general"):
+    str.replace("eboard in general", "Eboard in general");
     str.replace("evals","Evals");
     str.replace("financial","Financial");
     str.replace("history","History");

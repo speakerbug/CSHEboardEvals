ab8c12d1b99d32538ebaf38bdddbe5a79cb21f0b
harshisawesome <hxj6205@rit.edu>
2015-09-22 04:14:45 -0400

    Fixed syntax error
    Syntax error

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -5,13 +5,14 @@ $(document).ready(function() {
     $("#expandButton").click(function()
     {
         expanded = $(this).data('expanded');
-        if(expanded == false){
-        $(this).data('expanded',true)
-        console.log("Expanding");
-        $(".container").css('width', '100%');
-    }
+        if(expanded == 'false')
+        {
+            $(this).data('expanded','true')
+            console.log("Expanding");
+            $(".container").css('width', '100%');
+        }
         else{
-             $(this).data('expanded',false)
+             $(this).data('expanded','false')
         console.log("CLosing");
         $(".container").css('width', '100%');  
         }

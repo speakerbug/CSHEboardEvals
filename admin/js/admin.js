661dab7c128c8602a86fd1164c2d91015b2efff5
harshisawesome <hxj6205@rit.edu>
2015-09-22 04:11:29 -0400

    Toggle Expand
    Added toggle to expand button

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
+    
+    var origWidth;
+    
     $("#expandButton").click(function()
     {
+        expanded = $(this).data('expanded');
+        if(expanded == false){
+        $(this).data('expanded',true)
         console.log("Expanding");
         $(".container").css('width', '100%');
+    }
+        else{
+             $(this).data('expanded',false)
+        console.log("CLosing");
+        $(".container").css('width', '100%');  
+        }
     });   
 });

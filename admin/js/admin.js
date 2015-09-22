b2c70dc77f46debdce1d0155d3c99f295062f3f4
harshisawesome <hxj6205@rit.edu>
2015-09-22 12:55:23 -0400

    Add Expand/Collapse Text for table width button
    Adds that ^^

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -10,11 +10,13 @@ $(document).ready(function() {
             $(this).data('expanded','true')
             console.log("Expanding");
             $(".container").css('width', '100%');
+            $('#expandButton').text("Collapse");
         }
         else{
              $(this).data('expanded','false')
         console.log("CLosing");
         $('.container').css('width',initialWidth);
+            $('#expandButton').text("Expand");
         }
     });   
 });
